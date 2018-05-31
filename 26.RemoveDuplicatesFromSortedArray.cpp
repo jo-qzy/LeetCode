cbbeae14e8b2a3657bfad842f71763245ea459bb
jo-qzy <2651933495@qq.com>
2018-05-31 11:39:19 +0800

    删除排序数组中的重复元素

---
+++ 26.RemoveDuplicatesFromSortedArray.cpp
@@ -0,0 +1,15 @@
+int removeDuplicates(int* nums, int numsSize) {
+    int size = numsSize;
+    int slow = 0, fast =1;
+    while (fast < numsSize)
+    {
+		//慢指针每次只走一步，快指针找同慢指针不同的数，直接替换到慢指针后面的位置
+        while(nums[slow] == nums[fast] && fast < numsSize)
+        {
+            fast++;
+            size--;
+        }
+        nums[++slow] = nums[fast++];
+    }
+    return size;
+}
