9b5d5aab54193425fbbd5857cd34226db3f7e8db
jo-qzy <33571776+jo-qzy@users.noreply.github.com>
2018-06-09 14:57:51 +0800

    盛水最多的容器

--- 11.ContainerWithMostWater.cpp
+++ 11.ContainerWithMostWater.cpp
@@ -1,29 +1,29 @@
 int maxArea(int* height, int heightSize) {
-    int max = 0;
-    int left = 0;
-    int right = heightSize - 1;
-    int cur = 0;
+	int max = 0;
+	int left = 0;
+	int right = heightSize - 1;
+	int cur = 0;
 	//左右指针法，时间复杂度O(N)
-    while (left < right)
-    {
+	while (left < right)
+	{
 		//cur为当前面积
-        cur = (right - left) * (height[left] < height[right] ? height[left] : height[right]);
-        if (max < cur)
-        {
-            max = cur;
-        }
-        if (height[left] > height[right])
-        {
+		cur = (right - left) * (height[left] < height[right] ? height[left] : height[right]);
+		if (max < cur)
+		{
+			max = cur;
+		}
+		if (height[left] > height[right])
+		{
 			//当左边的高度大时，移动左边的边的面积只能是小于当前面积
 			//因为底 = 原来的底 - 1，高最大只能是右边的边的高
 			//所以只能移动右边的高
-            right--;
-        }
-        else
-        {
+			right--;
+		}
+		else
+		{
 			//分析同上
-            left++;
-        }
-    }
-    return max;
+			left++;
+		}
+	}
+	return max;
 }
