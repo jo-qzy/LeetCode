49de0cef60c4a78987487dc4a95e39ca3fdd1c53
jo-qzy <33571776+jo-qzy@users.noreply.github.com>
2018-06-09 15:50:25 +0800

    回文数

--- 9.PalindromeNumber.cpp
+++ 9.PalindromeNumber.cpp
@@ -1,21 +1,21 @@
 bool isPalindrome(int x) {
-    if (x < 0)
-    {
+	if (x < 0)
+	{
 		//需要翻转的数小于0，直接返回
-        return false;
-    }
-    int origin = x;//原始数据，一个原则，尽量不动参数
-    int reverse = 0;//翻转后的数据
-    while (origin != 0)
-    {
-        reverse *= 10;
-        reverse += origin % 10;
-        origin /= 10;
-    }
-    if (reverse == x)
-    {
+		return false;
+	}
+	int origin = x;//原始数据，一个原则，尽量不动参数
+	int reverse = 0;//翻转后的数据
+	while (origin != 0)
+	{
+		reverse *= 10;
+		reverse += origin % 10;
+		origin /= 10;
+	}
+	if (reverse == x)
+	{
 		//翻转后的数等于原数
-        return true;
-    }
-    return false;
+		return true;
+	}
+	return false;
 }
