f4d921d0cb3e4997267604755657e002c54038f7
jo-qzy <33571776+jo-qzy@users.noreply.github.com>
2018-06-09 15:38:45 +0800

    环形链表

--- 141.LinkedListCycle.cpp
+++ 141.LinkedListCycle.cpp
@@ -1,33 +1,33 @@
 /**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
+* Definition for singly-linked list.
+* struct ListNode {
+*     int val;
+*     ListNode *next;
+*     ListNode(int x) : val(x), next(NULL) {}
+* };
+*/
 class Solution {
 public:
-    bool hasCycle(ListNode *head) {
-		ListNode * fast = head, * slow = head;
-	    while (fast && fast->next)
-	    {
-		    //快指针走两步，慢指针走一步
-		    slow = slow->next;
-		    fast = fast->next->next;
-		    if (slow == fast)
-		    {
-			    fast = head;
-			    while (fast != slow)
-			    {
-				    //相遇点和从头开始走的指针同时走，走过的距离为链表长度的n倍
-				    //所以同时走相遇的位置就是入口
-				    slow = slow->next;
-				    fast = fast->next;
-			    }
-			    return fast;
-		    }
-	    }
-	    return NULL;
-    }
+	bool hasCycle(ListNode *head) {
+		ListNode * fast = head, *slow = head;
+		while (fast && fast->next)
+		{
+			//快指针走两步，慢指针走一步
+			slow = slow->next;
+			fast = fast->next->next;
+			if (slow == fast)
+			{
+				fast = head;
+				while (fast != slow)
+				{
+					//相遇点和从头开始走的指针同时走，走过的距离为链表长度的n倍
+					//所以同时走相遇的位置就是入口
+					slow = slow->next;
+					fast = fast->next;
+				}
+				return fast;
+			}
+		}
+		return NULL;
+	}
 };
