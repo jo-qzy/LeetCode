30cde7aee3b08621b574b8530c015ba94ac9743c
jo-qzy <2651933495@qq.com>
2018-06-24 23:36:12 +0800

    相同的树

--- 100.SameTree.cpp
+++ 100.SameTree.cpp
@@ -1,34 +1,49 @@
 /**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
+* Definition for a binary tree node.
+* struct TreeNode {
+*     int val;
+*     TreeNode *left;
+*     TreeNode *right;
+*     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+* };
+*/
 class Solution {
 public:
-    bool isSameTree(TreeNode* p, TreeNode* q) {
-        vector<TreeNode*> pa, qa;
-        getSequence(p, pa);
-        getSequence(q, qa);
-        if (pa.size() != qa.size())
-        {
-            return false;
-        }
-        for(int i = pa.size())
-    }
-    
+	bool isSameTree(TreeNode* p, TreeNode* q) {
+		vector<TreeNode*> pa, qa;
+		getSequence(p, pa);//得到p的前序
+		getSequence(q, qa);//得到q的前序
+		//比较序列
+		if (pa.size() != qa.size())
+		{
+			return false;
+		}
+		for (int i = 0; i < pa.size(); i++)
+		{
+			if (pa[i] && qa[i])
+			{
+				if (pa[i]->val != qa[i]->val)
+				{
+					return false;
+				}
+			}
+			else if (pa[i] || qa[i])
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+
 private:
-    void getSequence(TreeNode* root, vector<TreeNode*>& a)
-    {
-        a.push_back(root);
-        if (root == NULL)
-        {
-            return;
-        }
-        getSequence(root->left, a);
-        getSequence(root->right, a);
-    }
+	void getSequence(TreeNode* root, vector<TreeNode*>& a)
+	{
+		a.push_back(root);
+		if (root == NULL)
+		{
+			return;
+		}
+		getSequence(root->left, a);
+		getSequence(root->right, a);
+	}
 };
