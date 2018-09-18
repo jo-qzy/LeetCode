0a35e0645d770391621fb52498d2a33bb9c86802
jo-qzy <2651933495@qq.com>
2018-09-18 17:38:16 +0800

    最小栈

--- 155.MinStack.cpp
+++ 155.MinStack.cpp
@@ -75,3 +75,42 @@ private:
  * int param_3 = obj.top();
  * int param_4 = obj.getMin();
  */
+ 
+ 
+ //2018/9/18，九一八事变，缅怀一下先烈，勿忘国耻，振兴中华
+ //添加第二种写法，思路一样，赋用stack模板更简单
+ class MinStack {
+public:
+    /** initialize your data structure here. */
+
+    void push(int value) {
+        _stack.push(value);
+        if (_min.size() == 0 || value <= _min.top())
+            _min.push(value);
+    }
+    void pop() {
+        if (_min.top() == _stack.top())
+            _min.pop();
+        _stack.pop();
+    }
+    int top() {
+        if (_stack.size() != 0)
+            return _stack.top();
+    }
+    int getMin() {
+        if (_min.size() != 0)
+            return _min.top();
+    }
+private:
+    stack<int> _stack;
+    stack<int> _min;
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(x);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
